c84f79dab6c9045d4610385491064a6cc3ae0901
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-03-01 09:43:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import re, os, time
 import datetime
 
 class Config:
-    BOT_TOKEN = environ.get('BOT_TOKEN')
-    API_HASH = environ.get('API_HASH')
-    API_ID = int(environ.get('API_ID'))
-    OWNER_ID = int(environ.get('OWNER_ID')) #Admin UserId
-    DB_URI = environ.get('DB_URI') #May Be In Future.
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    API_HASH = os.environ.get('API_HASH')
+    API_ID = int(os.environ.get('API_ID'))
+    OWNER_ID = int(os.environ.get('OWNER_ID')) #Admin UserId
+    DB_URI = os.environ.get('DB_URI') #May Be In Future.
     
