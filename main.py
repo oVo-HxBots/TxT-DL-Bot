193e6d2423002979e38de95bdb5a990b99245676
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-03-01 09:35:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def cancel(_, m):
     await editable.edit("cancled")
     return
     
-@bot.on_message(filters.command(["restart"]))
+@bot.on_message(filters.command(["restart"]) & filters.user(Config.OWNER_ID))
 async def restart_handler(_, m):
     await m.reply_text("Bot Restarted! ♻️", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
